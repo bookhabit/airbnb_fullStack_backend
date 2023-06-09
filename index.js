ddf57292114e11d1bdc45d190310f053d20cf08c
bookhabit <qnrgoqlt@naver.com>
2023-06-09 17:29:54 +0900

    회원가입기능

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const User = require('./models/User.js');
+const bcrypt = require('bcryptjs')
+
 require('dotenv').config()
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+const bcryptSalt = bcrypt.genSaltSync(10);
 
-console.log(process.env.MONGO_URL)
+app.use(express.json())
+app.use(cors({credentials:true,origin:'http://localhost:5173'}));
+
 // 몽고 db password ff16ouQEJJcn82YP
 mongoose.connect(process.env.MONGO_URL);
-
-app.post('/register',(req,res)=>{
+  
+app.post('/register', async (req,res) => {
+mongoose.connect(process.env.MONGO_URL);
     const {name,email,password} = req.body;
-    res.json({name,email,password})
-})
+    try {
+        const userDoc = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+});
 
 app.listen(4000)
