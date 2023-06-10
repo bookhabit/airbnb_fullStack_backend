58b86e9f028f740b873220b5c1f1ffe06b4507cd
bookhabit <qnrgoqlt@naver.com>
2023-06-10 18:21:10 +0900

    파일업로드

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const User = require('./models/User.js');
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
+const imageDownloader = require('image-downloader')
+const multer = require('multer')
+const fs = require('fs')
 
 require('dotenv').config()
 const app = express();
@@ -15,6 +18,7 @@ const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 
 app.use(express.json())
 app.use(cookieParser());
+app.use('/uploads/',express.static(__dirname+'/uploads'))
 app.use(cors({credentials:true,origin:'http://localhost:5173'}));
 
 app.post('/register', async (req,res) => {
@@ -70,6 +74,30 @@ if (token) {
 
 app.post('/logout',(req,res)=>{
     res.cookie('token','').json(true);
+})
+
+app.post('/upload-by-link',async(req,res)=>{
+    const {link} = req.body;
+    const newName = 'photo'+ Date.now() + '.jpg'
+    await imageDownloader.image({
+      url:link,
+      dest:__dirname+'/uploads'+newName,
+    })
+    res.json(newName)
+})
+
+const photosMiddleware = multer({dest:'uploads/'})
+app.post('/upload',photosMiddleware.array('photos',100),(req,res)=>{
+  const uploadFiles = [];
+  for(let i=0;i<req.files.length;i++){
+    const {path,originalname} = req.files[i];
+    const parts = originalname.split('.');
+    const ext = parts[parts.length-1]
+    const newPath = path +'.'+ext;
+    fs.renameSync(path,newPath)
+    uploadFiles.push(newPath.replace('uploads/',''));
+  }
+  res.json(uploadFiles);
 })
 
 app.listen(4000)
