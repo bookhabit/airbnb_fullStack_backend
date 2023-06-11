408d00a0c4da818f999ab4784fdd7041a3f80e9c
bookhabit <qnrgoqlt@naver.com>
2023-06-12 04:49:50 +0900

    유저의 숙소예약리스트 api

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ app.use(cookieParser());
 app.use('/uploads/',express.static(__dirname+'/uploads'))
 app.use(cors({credentials:true,origin:'http://localhost:5173'}));
 
+function getUserDataFromReq(req){
+  return new Promise((resolve,reject)=>{
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if(err) throw err;
+      resolve(userData);
+    })
+  })
+}
+
 app.post('/register', async (req,res) => {
 mongoose.connect(process.env.MONGO_URL);
     const {name,email,password} = req.body;
@@ -170,16 +179,27 @@ app.get('/places',async (req,res)=>{
 
 app.post('/bookings', async (req, res) => {
   mongoose.connect(process.env.MONGO_URL);
+  const userData = await getUserDataFromReq(req);
   const {
     place,checkIn,checkOut,numberOfGuests,name,phone,price,
   } = req.body;
   Booking.create({
     place,checkIn,checkOut,numberOfGuests,name,phone,price,
+    user:userData.id,
   }).then((doc) => {
     res.json(doc);
   }).catch((err) => {
     throw err;
   });
 });
+
+app.get('/bookings',async (req,res)=>{
+  mongoose.connect(process.env.MONGO_URL);
+  const userData = await getUserDataFromReq(req)
+  const bookingList = await Booking.find({user:userData.id}).populate('place')
+  console.log(bookingList)
+  res.json(bookingList)
+})
+
 
 app.listen(4000)
